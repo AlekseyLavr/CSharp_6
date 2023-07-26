ea98c85e754170aa89d1e4c60f9dd90fc64af130
AlekseyLavr <govola77@gmail.com>
2023-07-26 19:36:28 +0300

    Read

--- DZ_6_41/Program.cs
+++ DZ_6_41/Program.cs
@@ -8,7 +8,6 @@ int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int count = 0;
 for (int i = 0; i < array.Length; i++)
 {
-    
     if (array[i] > 0)
         count++;
 }
