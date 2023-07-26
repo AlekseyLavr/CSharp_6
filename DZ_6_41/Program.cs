70928e5e36b9f4fa6de7e2d76fe109f94712052d
AlekseyLavr <govola77@gmail.com>
2023-07-26 19:26:16 +0300

    Домашнее задание 41

---
+++ DZ_6_41/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+Console.Clear();
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    
+    if (array[i] > 0)
+        count++;
+}
+Console.WriteLine($"Чисел больше нуля: {count}");
